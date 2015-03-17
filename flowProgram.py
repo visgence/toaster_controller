c1ed6d40f85de0695abcfafa8a6fe2817ecf2f6b
Deanna Jumbo <djumbo@visgence.com>
2015-03-17 15:36:35 -0600

    Modified flowProgram with arg for profile,
    open profile and stored in dict, plotted profile with matplotlib, also
    stored json data in profile file.  Made new python file
    toaster_controller.py with controller code to later add flowProgram
    modifications.

--- flowProgram.py
+++ flowProgram.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
+import json
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
 
+from matplotlib import pylab
 #
 # README
 # 
@@ -12,6 +17,18 @@
 #
 #the example profile is based on Kester EP256
 #
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--profile', help="Input Profile File")
+args = parser.parse_args()
+
+print "profile file : %s " % args.profile
+
+with open(args.profile) as f:
+    setpoints = json.load(f)
+f.close()
+
+"""
 setpoints = [{'t_plus':0
               ,'temp':20}
             ,{'t_plus':90
@@ -30,10 +47,16 @@ setpoints = [{'t_plus':0
               ,'temp':50
               }
             ]
+"""
+
+#fig = plt.figure()
+#ax1 = fig.add_subplot(111)
 
 time = 0
 prev_temp = setpoints[0]['temp']
 prev_time = setpoints[0]['t_plus']
+
+data = {}
 
 for i in range(1, len(setpoints)):
     #print setpoints[i] #DEBUG
@@ -41,13 +64,26 @@ for i in range(1, len(setpoints)):
     #print slope #DEBUG
     time = prev_time
     temp = prev_temp
+    
     while time < setpoints[i]['t_plus']:
         temp += slope
         time += 1
         print "%d,%d" % (time, temp)
-     
+        data[time]=int(temp)
+        plt.plot(time,temp, 'bo')
+
+
     prev_temp = setpoints[i]['temp']
     prev_time = setpoints[i]['t_plus']
-   
 
 
+plt.show()
+#generates a json? profile
+with open('curve.json','w') as f:
+    json.dump(data,f)
+f.close()
+
+#create profile template
+#with open('test.json','w') as f:
+#  json.dump(setpoints,f)
+#f.close()
---
+++ toaster_controller.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import sys
+import struct
+import time
+import serial
+import pid2
+import time
+import argparse
+import json
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', help="Input Serial Port", required = True)
+parser.add_argument('--profile', help="Input Profile File")
+args = parser.parse_args()
+
+print "port is : %s " % args.port
+print "profile file : %s " % args.profile
+
+if __name__ == "__main__":
+
+    output = 1
+    wait = 0
+    
+    set = 0
+
+    serial = serial.Serial(port=args.port,baudrate=9600)
+    
+    time.sleep(2)
+    print "Start"
+    
+    p = pid2.PID(2,0.05,0.5,int_max=640,int_min=0)
+    p.setPoint(0)
+
+    #read file
+    file = open(sys.argv[1],'r')
+    start_time = time.time()
+
+
+    (set_time,set_temp,msg) = file.readline().rstrip().split(",")
+    #serial.write(struct.pack("B",int(set_temp))    
+    
+#    while(1):
+
+#        p.setPoint(float(set_temp))
+           
+#        temp = float(serial.readline().strip());
+        
+#        pid = p.update(temp)
+#        if(pid>32):
+#            output = 32
+#        elif(pid<0):
+#            output = 0
+#        else:
+#            output = int(pid)
+        
+#        print "Time " + set_time + " Temp: " + str(temp) + " "  + "Set_temp: " + set_temp  +" PID: " + str(output) + " PID: " + str(pid) + " " + msg
+
+#        serial.write(struct.pack("B",output))
+
+#        if(int(set_time)+start_time <= time.time()):
+#            (set_time,set_temp,msg) = file.readline().rstrip().split(",")
+
+
+
+#graph generated profile
